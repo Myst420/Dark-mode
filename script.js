8585a69298fc00fb9054660d97849a3a0b06b1fa
Myst420 <84757484+Myst420@users.noreply.github.com>
2021-07-24 16:55:33 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', function myFunction() {
+    const bdy2 = document.getElementById('bdy2')
+    bdy2.classList.toggle("bdy");
+  });
